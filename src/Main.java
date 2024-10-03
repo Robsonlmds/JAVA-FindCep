fa146e29c50506977166aa3ff4bca87001dcd879
Robson Lucas Messias <98341530+Robsonlmds@users.noreply.github.com>
2024-10-03 14:09:34 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 package PACKAGE_NAME;
 
 public class main {
+     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        CheckCep CheckCep = new CheckCep();
+
+        System.out.println("Digite um número de CEP para consulta:");
+        var cep = read.nextLine();
+
+        try {
+            Endereco newAddress = CheckCep.buscaEndereco(cep);
+            System.out.println(newAddress);
+            GenerateJson generate = new GenerateJson();
+            generate.salvaJson(newAddress);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+    }
 }
