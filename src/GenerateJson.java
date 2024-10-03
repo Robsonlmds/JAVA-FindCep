66ccfcd16a5b5b4fe6b109196436918731d51bc8
Robson Lucas Messias <98341530+Robsonlmds@users.noreply.github.com>
2024-10-03 14:07:18 -0300

    Update GenerateJson.java

--- src/GenerateJson.java
+++ src/GenerateJson.java
@@ -1,4 +1,15 @@
 package PACKAGE_NAME;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class GenerateJson {
+  public void SaveJson (address address) throws IOException {
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+          FileWriter writing = new FileWriter(address.cep() + ".json");
+          writing.write(gson.toJson(address));
+          writing.close();
+  }
 }
