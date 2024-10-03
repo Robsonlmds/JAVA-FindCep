2b22636de66fd8adf3c13c0d6e11736d22b6ed31
Robson Lucas Messias <98341530+Robsonlmds@users.noreply.github.com>
2024-10-03 14:03:20 -0300

    Update CheckCep.java

--- src/CheckCep.java
+++ src/CheckCep.java
@@ -1,4 +1,30 @@
 package PACKAGE_NAME;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CheckCep {
+
+    public Address buscaAddress(String cep) {
+        URI Address = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(Address)
+                    .build();
+
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Address.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Não consegui obter o endereço a partir desse CEP.");
+        }
+
+    }
 }
